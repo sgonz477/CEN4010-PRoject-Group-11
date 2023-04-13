5c212936a45ed026416c386109d5e5795f591a96
vanesagomez <vgom090@fiu.edu>
2023-04-12 21:59:21 -0400

    Book Browsing & Sorting - Final Code

--- src/main/java/com/gomez/api/rest/Repo/BookRepo.java
+++ src/main/java/com/gomez/api/rest/Repo/BookRepo.java
@@ -1,15 +1,16 @@
 package com.gomez.api.rest.Repo;
-
 import com.gomez.api.rest.Model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 
 public interface BookRepo extends JpaRepository<Book, Long> {
-    Book getBooksByGenre(String genre);
 
     List<Book> findTop10ByOrderByCopiesSoldDesc();
 
+    List<Book> findByRatingGreaterThanEqual(int rating);
 
+    List<Book> findByGenre(String genre);
+
+    List<Book> findByPublisher(String publisher);
 }
